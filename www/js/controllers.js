7c715b73bb2d304207a541919256901b0fad64a4
Jackie <jackie.lam@capgemini.com>
2016-09-21 20:57:50 +0200

    added functionalities

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,7 +42,7 @@ angular.module('starter.controllers', [])
     };
 
     $scope.percentage = function() {
-      if($scope.answer === 3388) {
+      if($scope.answer === 8) {
         $state.go('tab.chats');
       }
     }
@@ -64,7 +64,116 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ChatsCtrl', function($scope,$state) {
+.controller('ChatsCtrl', function($scope,$state,$ionicModal,$ionicSlideBoxDelegate, $cordovaDevice, $cordovaFile, $ionicPlatform, $cordovaEmailComposer, $ionicActionSheet, ImageService, FileService) {
+
+  // $ionicPlatform.ready(function() {
+  $scope.images = FileService.images();
+    // $scope.$apply();
+  // });
+
+  // $scope.image1 = $scope.images[0];
+
+  $scope.urlForImage = function(imageName) {
+    var trueOrigin = cordova.file.dataDirectory + imageName;
+    return trueOrigin;
+  }
+
+  $scope.addMedia = function() {
+    $scope.hideSheet = $ionicActionSheet.show({
+      buttons: [
+        { text: 'Take photo' },
+        { text: 'Photo from library' }
+      ],
+      titleText: 'Add images',
+      cancelText: 'Cancel',
+      buttonClicked: function(index) {
+        $scope.addImage(index);
+      }
+    });
+  }
+
+  $scope.addImage = function(type) {
+    $scope.hideSheet();
+    ImageService.handleMediaDialog(type).then(function() {
+      $scope.$apply();
+    });
+  }
+
+  $scope.aImages = [{
+      	'src' : 'img/jane.jpg',
+      	'msg' : 'Swipe me to the left. Tap/click to close'
+    	}, {
+        'src' : 'img/chloe.jpg',
+        'msg' : ''
+      }, {
+        'src' : 'img/mary.jpg',
+        'msg' : ''
+      }, {
+        'src' : 'img/denise.jpg',
+        'msg' : ''
+      }, {
+        'src' : 'img/doutzen.jpg',
+        'msg' : ''
+      }, {
+        'src' : 'img/suzy.jpg',
+        'msg' : ''
+      }, {
+        'src' : 'img/mary.jpg',
+        'msg' : ''
+      }
+  ];
+
+    $ionicModal.fromTemplateUrl('image-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+      $ionicSlideBoxDelegate.slide(0);
+      $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hide', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+    $scope.$on('modal.shown', function() {
+      console.log('Modal is shown!');
+    });
+
+    // Call this functions if you need to manually control the slides
+    $scope.next = function() {
+      $ionicSlideBoxDelegate.next();
+    };
+
+    $scope.previous = function() {
+      $ionicSlideBoxDelegate.previous();
+    };
+
+  	$scope.goToSlide = function(index) {
+      $scope.modal.show();
+      $ionicSlideBoxDelegate.slide(index);
+    }
+
+    // Called each time the slide changes
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+    };
+
 
   $scope.goBack = function() {
     $state.go('tab.dash');
